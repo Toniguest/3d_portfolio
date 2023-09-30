21691f89ced9806389ff5daf45f51d868a738609
Antonio Pirone <combo2100@gmail.com>
2023-09-30 02:17:43 +0200

    Completed about, to do the others, SectionWrapper implemented like hoc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
-
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return (
@@ -11,18 +20,18 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
