b5bfe482ece6caa652f51709672df1c07caead9d
Antonio Pirone <combo2100@gmail.com>
2023-10-03 20:24:24 +0200

    fix white screen

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
       frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      gl={{ preserveDrawingBuffer: true, alpha: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
